6e47291f104dfa14f96118e3f33f0e7e5a4c1cca
ArthurSauerr <germanooarthuur@gmail.com>
2024-04-05 10:59:48 -0300

    feat: text, button, switch

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button, Switch } from 'react-native';
 
 export default function App() {
+
+const [switchValue, setSwitchValue] = useState(false)
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button title='Teste'/>
+      <Switch 
+      value={switchValue}
+      onValueChange={() => setSwitchValue(!switchValue)} />
       <StatusBar style="auto" />
     </View>
   );
